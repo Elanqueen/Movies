b5b6cbbaccedc0e05957258dd902b40749c08573
Elanqueen <775254855@qq.com>
2018-02-08 15:18:02 +0800

    添加了影片海报显示功能

--- bin/app.py
+++ bin/app.py
@@ -1,10 +1,16 @@
-#coding=utf-8
+#-*- coding: utf-8 -*-
 import web
 #连接数据库
 import sqlite3
 
 import urllib
 import json
+import sys
+
+defaultencoding='utf-8'
+if sys.getdefaultencoding()!= defaultencoding:
+    reload(sys)
+    sys.setdefaultencoding(defaultencoding)
 
 urls=(
     '/','Index',
@@ -24,6 +30,8 @@ movies=[
      'year':1999}
 ]
 """
+#抛出异常设定
+class NullException(BaseException):"It is null, Please check."
 
 class Index:
     def GET(self):
@@ -37,10 +45,16 @@ class Index:
 
     def POST(self):
         db = sqlite3.connect("MovieSite.db")
-        form = web.input(name="title")
-        movie=db.execute("select * from movie where title = %s" % form.name).fetchone()
-        db.close()
-        return render.movie_des(movie)
+        db.text_factory=str
+        form = web.input(name="霸王别姬") # 默认值设置：name="霸王别姬"name='阿甘正传'
+        if form.title:
+            #movie=db.execute("select * from mvgrab where title = '霸王别姬'").fetchone()
+            # movie=db.execute("select * from mvgrab where title = %s" % form.title).fetchone()
+            movie=db.execute("select * from mvgrab where title = ?",(unicode(form.title),)).fetchone()
+            db.close()
+            return render.movie_des(movie)
+        else:
+            raise NullException
 
 class Movie:
     def GET(self,movie_id):
@@ -48,37 +62,56 @@ class Movie:
         cu.execute("select * from movie where id = ?",movie_id)
         movie=cu.fetchone()'''
         db = sqlite3.connect("MovieSite.db")
-        movie=db.execute("select * from movie where id = ?",movie_id).fetchone()
+        movie=db.execute("select * from mvgrab where mvid = ?",(movie_id,)).fetchone()
         db.close()
-        return render.movie_des(movie)
+        if movie:
+            return render.movie_des(movie)
+        else:
+            raise NullException
 
 class GrabData:
     """从豆瓣网站抓取排名前250的电影列表"""
     def GET(self):
-        fp = urllib.urlopen("https://api.douban.com/v2/movie/top250?strat=0&count=5")
-        data = fp.read()
-        # print data
-        data_js = json.loads(data)
-        movie_250 = data_js['subjects']
-        movies = [] #字典存储数据，页面显示
-        dat=[] #存储数据添加入数据库
-        for movie in movie_250:
-            movies.append({"id": movie['id'],
-                           "title": movie['title'],
-                           'alt':movie['alt'],
-                           'genres':movie['genres'],
-                           'casts':movie['casts'],
-                           })
-            #print movie['title'], movie['id']
-            dat.append((movie['id'],movie['title'],movie['alt'],movie['genres'],movie['casts']))
-        self.add_movie(dat)
+        self.grab_movie_to_db()
+        db = sqlite3.connect("MovieSite.db")
+        movies=db.execute("select * from mvgrab limit 10").fetchall()
+        db.close()
         return render.index(movies)
 
-    def add_movie(self,data):
+    def grab_movie_to_db(self):
+        '''将通过豆瓣API抓取到的排名前250的电影信息存入数据库'''
+        fp = urllib.urlopen("https://api.douban.com/v2/movie/top250?strat=0&count=50")
+        data = fp.read()
+        data_js = json.loads(data)
+        movie_250 = data_js['subjects']
+        movies = []  # 存储数据添加入数据库
+        count=1
+        for movie in movie_250:
+            #将图片存入本地,暂时屏蔽了下载功能
+            #self.get_poster(int(movie['id']),movie['images']['medium'])
+
+            genres = ','.join(movie['genres'])
+            casts = ','.join([d['name'] for d in movie['casts']])
+            directors = ','.join([d['name'] for d in movie['directors']])
+            movies.append((count,int(movie['id']), movie['title'], movie['rating']['average'],
+                        movie['original_title'], directors, movie['alt'],
+                        movie['year'], movie['subtype'].decode(), movie['images']['medium'],
+                        genres, casts
+                        ))
+            count=count+1
         db = sqlite3.connect("MovieSite.db")
-        db.executemany("insert into movie_grab values(?,?,?,?,?)",data)
+        db.execute("delete from mvgrab")
+        db.executemany("insert into mvgrab values(?,?,?,?,?,?,?,?,?,?,?,?)", movies)
         db.commit()
         db.close()
+
+    def get_poster(self,id,url):
+        '''将获取的图片存入本地'''
+        pic=urllib.urlopen(url).read()
+        fn='static/poster/%d.jpg' % id
+        f=open(fn,'wb')
+        f.write(pic)
+        f.close()
 
 if __name__=='__main__':
     app.run()
